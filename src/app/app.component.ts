2d75279884c0145fc38b5330f57da4b8b3a18423
Rei Lin <temisrei@gmail.com>
2017-09-15 23:16:03 +0800

    add Radio Buttom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
